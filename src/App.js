ee156422eda761a2958eb81aa1cf6e5ed8b30124
Pedro Palma <pedro.francisco.palma@gmail.com>
2025-11-24 21:36:50 -0300

    -fix: nav-bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import Index from './pages/Index';
 import Produtos from './pages/Produtos';
 
 export default function App() {
   return (
-    <Index />
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/produtos" element={<Produtos />} />
+    </Routes>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 import "../assets/css/css_components/Header.css";
 import logo from "../assets/img/logo3.png";
 import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
 // Function do Header
 function Header() {
@@ -11,10 +12,10 @@ function Header() {
                 <img src={logo} alt="Site do Chico" className="logo" />
                     <nav className={`nav-links ${open ? "active" : ""}`}>
                         <ul className="nav-list">
-                            <li><a href="index.html" class="nav-item">Home</a></li>
-                            <li><a href="produtos.html" class="nav-item">Produtos</a></li>
-                            <li><a href="contatos.html" class="nav-item">Contato</a></li>
-                            <li><a href="pedidos.html" class="nav-item">Pedidos</a></li>
+                            <li className="nav-item"><Link to="/">Inicio</Link></li>
+                            <li className="nav-item"><Link to="/produtos">Produtos</Link></li>
+                            <li className="nav-item"><a href="contatos.html">Contato</a></li>
+                            <li className="nav-item"><a href="pedidos.html">Pedidos</a></li>
                         </ul>
                     </nav>
                         <button className="menu-btn" onClick={() => setOpen(!open)}>
